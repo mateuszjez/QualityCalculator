41429cd0badb68b21beb5f62108d45281b0d16ae
mjez <mjez@agh.edu.pl>
2018-01-13 15:30:27 +0100

    Testy na zajęciach

--- app/src/androidTest/java/pl/edu/agh/qualitycalculator/MainActivityTest.java
+++ app/src/androidTest/java/pl/edu/agh/qualitycalculator/MainActivityTest.java
@@ -30,6 +30,33 @@ public class MainActivityTest {
         onView(withId(R.id.etNum2)).perform(click()).perform(typeText("4"));
         onView(withId(R.id.btnAdd)).perform(click());
         onView(withId(R.id.tvResult)).check(matches(withText("4.0 plus 4.0 gives value 8.0")));
-       }
+    }
+    @Test
+    public void testSubtract(){
+        //fail("Not implemented yet!");
+
+        onView(withId(R.id.etNum1)).perform(click()).perform(typeText("8"));
+        onView(withId(R.id.etNum2)).perform(click()).perform(typeText("5"));
+        onView(withId(R.id.btnSub)).perform(click());
+        onView(withId(R.id.tvResult)).check(matches(withText("8.0 minus 5.0 gives value 3.0")));
+    }
+    @Test
+    public void testMultiply(){
+        //fail("Not implemented yet!");
+
+        onView(withId(R.id.etNum1)).perform(click()).perform(typeText("3"));
+        onView(withId(R.id.etNum2)).perform(click()).perform(typeText("2"));
+        onView(withId(R.id.btnMult)).perform(click());
+        onView(withId(R.id.tvResult)).check(matches(withText("3.0 multiplied by 2.0 gives value 6.0")));
+    }
+//    @Test
+//    public void testSubtract(){
+//        //fail("Not implemented yet!");
+//
+//        onView(withId(R.id.etNum1)).perform(click()).perform(typeText("8"));
+//        onView(withId(R.id.etNum2)).perform(click()).perform(typeText("5"));
+//        onView(withId(R.id.btnSub)).perform(click());
+//        onView(withId(R.id.tvResult)).check(matches(withText("8.0 minus 5.0 gives value 3.0")));
+//    }
 }
 
