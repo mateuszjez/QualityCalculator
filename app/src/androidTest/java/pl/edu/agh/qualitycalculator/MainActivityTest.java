2179badbde773ff21a0a0b086f493effa7b19b1a
mateuszjez <mateuszjez@gmail.com>
2018-01-17 15:21:00 +0100

    Add files via upload

--- app/src/androidTest/java/pl/edu/agh/qualitycalculator/MainActivityTest.java
+++ app/src/androidTest/java/pl/edu/agh/qualitycalculator/MainActivityTest.java
@@ -82,6 +82,14 @@ public class MainActivityTest {
         onView(withId(R.id.etNum2)).perform(click()).perform(typeText("6"));
         onView(withId(R.id.btnAv)).perform(click());
         onView(withId(R.id.tvResult)).check(matches(withText("Enter some data to calculate")));
+    }@Test
+    public void testDivideByZero(){
+        //fail("Not implemented yet!");
+        onView(withId(R.id.etNum1)).perform(click()).perform(typeText("3"));
+        onView(withId(R.id.etNum2)).perform(click()).perform(typeText("0"));
+        onView(withId(R.id.btnDiv)).perform(click());
+        onView(withId(R.id.tvResult)).check(matches(withText("An error ocurred: Division by zero")));
     }
+
 }
 
