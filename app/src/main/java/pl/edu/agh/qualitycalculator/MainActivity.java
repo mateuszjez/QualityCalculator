6eed9b8cc3c850d1585a2b944850f7f82c4fe3a7
mateuszjez <mateuszjez@gmail.com>
2018-01-17 15:19:30 +0100

    Add files via upload

--- app/src/main/java/pl/edu/agh/qualitycalculator/MainActivity.java
+++ app/src/main/java/pl/edu/agh/qualitycalculator/MainActivity.java
@@ -100,7 +100,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
             tvResult.setText(verbalizedOperation);
         }
         catch (Exception ex) {
-            tvResult.setText("An error ocurred: " + ex.toString());
+            tvResult.setText("An error ocurred: " + ex.getMessage());
         }
 
     }
